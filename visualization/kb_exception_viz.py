49afe6ae4d0205a1e0c498a7b5f529825345a5d3
Vassilis Markos <51245808+VMarkos@users.noreply.github.com>
2022-02-08 15:55:25 +0200

    Add files via upload

---
+++ visualization/kb_exception_viz.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import json
+
+'''
+KB DATA STRUCTURE:
+[
+  {
+    "name": "r0",
+    "body": [
+      "p1126",
+      "p752",
+      "p1240"
+    ],
+    "head": "p324"
+  },
+]
+'''
+
+def kb_to_graph(kb_object):
+    for rule in kb_object:
+        pass
+    pass # TODO Add edges one at a time.
+
+def visualize_kb(kb):
+    pass
+
+if __name__ == '__main__':
+    pass
