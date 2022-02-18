0e0447fe10c0bfcdd3df66ecb312580bc1e6cb05
Vassilis Markos <51245808+VMarkos@users.noreply.github.com>
2022-02-18 20:18:32 +0200

    Add files via upload

--- kb_generator/utils.js
+++ kb_generator/utils.js
@@ -10,6 +10,8 @@ let currentCy = undefined;
 let drawingArrow = false;
 const edges = new Map();
 const edgesTo = new Map();
+const kb = new Map(); // Div elements ids as keys and {name: body: head: } as value.
+let premiseCount = 0;
 
 /*
 TODO list:
@@ -31,7 +33,7 @@ window.addEventListener("mousedown", garbageCollector, false);
 svgContainer.addEventListener("mousemove", moveException, false);
 
 function moveException(event) {
-    console.log("Drawing?", drawingArrow);
+    // console.log("Drawing?", drawingArrow);
     const rect = event.target.getBoundingClientRect();
     const mouseX = event.pageX - rect.left;// + window.scrollY;
     const mouseY = event.pageY - rect.top;// - window.scrollX;
@@ -61,13 +63,13 @@ function garbageCollector(event) {
     if (pendingException) {
         pendingException = false;
     }
-    if (drawingArrow) {
-        console.log(event.target);
-    }
+    // if (drawingArrow) {
+    //     // console.log(event.target);
+    // }
 }
 
 function nodeContextMenu(event) {
-    console.log("Drawing in context?", drawingArrow);
+    // console.log("Drawing in context?", drawingArrow);
     activeContextMenu = true;
     const rect = event.target.getBoundingClientRect();
     const node = event.target.parentElement;
@@ -86,7 +88,7 @@ function nodeContextMenu(event) {
         optionElement.className = "context-menu-item-container";
         optionElement.id = option.replaceAll(" ", "-").replaceAll(".", "");
         optionElement.innerHTML = option;
-        console.log(optionElement.id);
+        // console.log(optionElement.id);
         contextMenu.appendChild(optionElement);
     }
     document.getElementById("Delete").onclick = function() {deleteNode(node);};
@@ -99,7 +101,7 @@ function addExistingException(event, startNode) {
     activeContextMenu = false;
     document.getElementById("context-menu").remove();
     let userInput = prompt("Enter existing rule's label:");
-    const ruleNumber = userInput.trim().substring(1);
+    const ruleNumber = userInput.trim().substring(1); // FIXME When the prompt box is 'Cancel'-ed, you get an error here (no element created).
     let nodeLabel = "node-" + ruleNumber;
     while (!document.getElementById(nodeLabel)) {
         userInput = prompt("Enter existing rule's label:");
@@ -113,17 +115,20 @@ function addExistingException(event, startNode) {
     const theta = Math.atan((endY - startY) / (endX - startX));
     const endShorten = Math.sign(endX - startX) * (nodeRadius + 16);
     const startShorten = -Math.sign(endX - startX) * nodeRadius;
+    const edgeLabel = "arrow-" + startNode.id + "-node-" + ruleNumber;
+    movingExceptionId = edgeLabel;
     drawArrow(startX - startShorten * Math.cos(theta), startY - startShorten * Math.sin(theta), endX - endShorten * Math.cos(theta), endY - endShorten * Math.sin(theta));
+    // console.log(edgeLabel);
     if (edges.has(startNode.id)) {
-        edges.get(startNode.id).push(movingExceptionId);
+        edges.get(startNode.id).push(edgeLabel);
     } else {
-        edges.set(startNode.id, [movingExceptionId]);
+        edges.set(startNode.id, [edgeLabel]);
     }
-    edges.get(startNode.id).push(movingExceptionId);
+    // edges.get(startNode.id).push(edgeLabel);
     if (edgesTo.has("node-" + ruleNumber)) {
-        edgesTo.get("node-" + ruleNumber).push(movingExceptionId);
+        edgesTo.get("node-" + ruleNumber).push(edgeLabel);
     } else {
-        edgesTo.set("node-" + ruleNumber, [movingExceptionId]);
+        edgesTo.set("node-" + ruleNumber, [edgeLabel]);
     } // FIXME THere is some cannot read props error here...
 }
 
@@ -139,7 +144,8 @@ function addException(event, startNode) {
     const dy = mouseY - startNodeItem.getAttribute("cy");
     const theta = Math.atan(dy / dx);
     const normalizedRadius = - Math.sign(dx) * nodeRadius;
-    movingExceptionId = "arrow-" + ruleCount;
+    movingExceptionId = "arrow-" + startNode.id + "-node-" + ruleCount;
+    // console.log(movingExceptionId);
     if (edges.has(startNode.id)) {
         edges.get(startNode.id).push(movingExceptionId);
     } else {
@@ -158,11 +164,25 @@ function deleteNode(node) {
     activeContextMenu = false;
     deactivatingClick = false;
     node.remove();
-    for (const edge of edges.get(node.id)) {
-        document.getElementById(edge).remove();
+    // console.log("edges: ", edges.get(node.id));
+    const nodeIdRE = /node-\d+/g;
+    if (edges.has(node.id)) {
+        for (const edge of edges.get(node.id)) {
+            const tempEndNode = edge.match(nodeIdRE)[1];
+            edgesTo.get(tempEndNode).splice(edgesTo.get(tempEndNode).indexOf(edge), 1);
+            document.getElementById(edge).remove();
+        }
+        edges.delete(node.id);
     }
-    for (const edge of edgesTo.get(node.id)) {
-        document.getElementById(edge).remove();
+    if (edgesTo.has(node.id)) {
+        // console.log("edgesTo.get(node.id)):", edgesTo.get(node.id));
+        for (const edge of edgesTo.get(node.id)) {
+            const tempStartNode = edge.match(nodeIdRE)[0];
+            edges.get(tempStartNode).splice(edges.get(tempStartNode).indexOf(edge), 1);
+            // console.log("edgeTo (edge):", edge);
+            document.getElementById(edge).remove();
+        }
+        edgesTo.delete(node.id);
     }
 }
 
@@ -173,17 +193,173 @@ function updateViewBox() {
 }
 
 function constructKb() {
-    return;
+    const rootNodes = [];
+    const headMap = new Map();
+    for (const node of edges.keys()) {
+        if (!edgesTo.has(node)) {
+            rootNodes.push(node);
+        }
+    }
+    if (rootNodes.length === 0) {
+        return false;
+    }
+    for (const root of rootNodes) {
+        forwardPropagation(root, headMap);
+    }
+    // console.log("headMap:", headMap);
+    adjustHeads(headMap);
+    const kbList = sortBySpecificity();
+    console.log(vKbToString(kbList));
+    const kbString = "@KnowledgeBase\n" + vKbToString(kbList);
+    const kbPrudens = parseKB(kbString);
+    return {
+        kbString: kbString,
+        kbSimple: kbList,
+        kbFull: kbPrudens,
+    };
 }
 
-function generateException(rule) {
-    return;
+function addStandaloneRule(nodeName) {
+    const body = []
+    const rootNodeSize = 2; // 3 is an arbitrary constant.
+    for (let i=0; i<rootNodeSize; i++) {
+        body.push("p" + premiseCount);
+        premiseCount++;
+    }
+    const head = "p" + premiseCount;
+    premiseCount++;
+    const name = "R" + nodeName.substring(nodeName.indexOf("-") + 1);
+    kb.set(nodeName, {
+        name: name,
+        body: body,
+        head: head,
+    })
+}
+
+function generateException(rule, nodeName, priorBody) {
+    const newName = "R" + nodeName.substring(nodeName.indexOf("-") + 1);
+    const negatedHead = negateHead(rule["head"]);
+    let newBody = rule["body"];
+    if (priorBody.length > 0) {
+        newBody = newBody.concat(priorBody);
+    } else {
+        newBody = newBody.concat(["p" + premiseCount]);
+        premiseCount++;
+    }
+    kb.set(nodeName, {
+        name: newName,
+        body: newBody,
+        head: negatedHead,
+    });
+}
+
+function forwardPropagation(nodeName, headMap) {
+    addStandaloneRule(nodeName);
+    const front = [nodeName];
+    const nodeIdRE = /node-\d+/g;
+    while (front.length > 0) {
+        const currentNode = front.pop();
+        // console.log("currentNode:", currentNode);
+        if (!edges.has(currentNode)) {
+            continue;
+        }
+        // console.log("edges:", edges);
+        for (const edge of edges.get(currentNode)) {
+            // console.log("edge:", edge);
+            const childNodeName = edge.match(nodeIdRE)[1];
+            if (kb.has(childNodeName)) {
+                const oldHead = kb.get(childNodeName)["head"];
+                generateException(kb.get(currentNode), childNodeName, kb.get(childNodeName)["body"]); // TODO Potentail issues with heads here!
+                // console.log("childNodeName:", childNodeName, "\ncurrentNode:", currentNode);
+                // console.log("KB:", kbToString(kb));
+                if (!headMap.has(oldHead)) {
+                    headMap.set(oldHead, negateHead(kb.get(currentNode)["head"]));
+                }
+            } else {
+                generateException(kb.get(currentNode), childNodeName, []);
+            }
+            front.push(childNodeName);
+        }
+    }
+}
+
+function adjustHeads(headMap) { // FIXME Greedy implementation, needs to be refined.
+    for (let oldHead of headMap.keys()) {
+        // console.log("(iter) headMap:", headMap, "\noldHead:", oldHead);
+        let newHead = headMap.get(oldHead);
+        const newSign = newHead.charAt(0) === "-";
+        const oldSign = oldHead.charAt(0) === "-";
+        if (oldHead.charAt(0) === "-") {
+            oldHead = oldHead.substring(1);
+        }
+        if (newHead.charAt(0) === "-") {
+            newHead = newHead.substring(1);
+        }
+        if (newSign === oldSign) {
+            for (const node of kb.keys()) {
+                const nodeHead = kb.get(node)["head"];
+                if (nodeHead === oldHead || nodeHead === negateHead(oldHead)) {
+                    kb.get(node)["head"] = nodeHead.replace(oldHead, newHead);    
+                }
+            }
+        } else {
+            for (const node of kb.keys()) {
+                const nodeHead = kb.get(node)["head"];
+                console.log("nodeHead:", nodeHead);
+                if (nodeHead === oldHead || nodeHead === negateHead(oldHead)) {
+                    // console.log("newHead:", newHead);
+                    // nodeHead = nodeHead.replace(oldHead, newHead);
+                    // console.log("(replace) nodeHead:", nodeHead);
+                    kb.get(node)["head"] = negateHead(nodeHead).replaceAll(oldHead, newHead);
+                }
+            }
+        }
+    }
+}
+
+function sortBySpecificity() { // FIXME Dummy sorting algorithm.
+    const kbList = [];
+    for (const rule of kb.values()) {
+        kbList.push(rule);
+    }
+    console.log("kbList:", kbList);
+    return kbList.sort((a, b) => {return a["body"].length - b["body"].length;});
+}
+
+function negateHead(head) {
+    if (head.charAt(0) === "-") {
+        return head.substring(1);
+    }
+    return "-" + head;
+}
+
+function vRuleToString(rule) {
+    let outputString = rule["name"] + " :: ";
+    for (let i=0; i<rule["body"].length; i++) {
+        const literal = rule["body"][i];
+        if (i < rule["body"].length - 1) {
+            outputString += literal + ", ";
+        } else {
+            outputString += literal + " implies " + rule["head"] + ";";
+        }
+    }
+    return outputString;
+}
+
+function vKbToString(kbList) {
+    let outputString = "";
+    console.log("kb:", kbList);
+    for (const rule of kbList) {
+        console.log("rule:", rule);
+        outputString += vRuleToString(rule) + "\n";
+    }
+    return outputString;
 }
 
 function placeNode(event) {
     // console.log(event.target);
-    // console.log("Pending outside:", pendingException); // TODO Why is pending exception false?
-    console.log("(ext) Drawing?", drawingArrow);
+    // console.log("Pending outside:", pendingException);
+    // console.log("(ext) Drawing?", drawingArrow);
     if (event.target.id === "grid-rect" && !activeContextMenu && !deactivatingClick) {
         // console.log("Pending inside:", pendingException);
         const rect = event.target.getBoundingClientRect();
@@ -192,7 +368,7 @@ function placeNode(event) {
         let shiftX = 0;
         let shiftY = 0;
         if (drawingArrow) {
-            console.log("pending");
+            // console.log("pending");
             const arrow = document.getElementById(movingExceptionId)
             const dx = mouseX - arrow.getAttribute("x1");
             const dy = mouseY - arrow.getAttribute("y1");
@@ -200,11 +376,11 @@ function placeNode(event) {
             const shortenLength = Math.sign(dx) * (nodeRadius + 12);
             shiftX = shortenLength * Math.cos(theta);
             shiftY = shortenLength * Math.sin(theta);
-        }
-        if (edgesTo.has("node-" + ruleCount)) {
-            edgesTo.get("node-" + ruleCount).push(movingExceptionId);
-        } else {
-            edgesTo.set("node-" + ruleCount, [movingExceptionId]);
+            if (edgesTo.has("node-" + ruleCount)) {
+                edgesTo.get("node-" + ruleCount).push(movingExceptionId);
+            } else {
+                edgesTo.set("node-" + ruleCount, [movingExceptionId]);
+            }
         }
         addNodeAt(mouseX + shiftX, mouseY + shiftY, nodeRadius, ruleCount);
         ruleCount++;
@@ -272,4 +448,14 @@ function nodeClick(event) {
     const mouseX = event.pageX - rect.left;// + window.scrollY;
     const mouseY = event.pageY - rect.top;// - window.scrollX;
     // console.log(mouseX, mouseY);
+}
+
+function download(filename, content) {
+    let element = document.createElement("a");
+    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(content));
+    element.setAttribute("download", filename);
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
